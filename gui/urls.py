361e5d8de706b79a21f40fbd9540fe1535ff985b
64BitUniverse <64bituniverse@gmail.com>
2024-05-28 14:31:52 +0200

    Removed Django

--- gui/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-from .views import sendCommands
-
-urlpatterns = [
-    path("", sendCommands().home, name="base"),
-    path("index.html", sendCommands().send, name="index"),
-]
